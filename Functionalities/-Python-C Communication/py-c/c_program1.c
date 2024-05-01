497adb97d680073835fe16bd9f950abf351d9860
th33k <theekshana.uda@gmail.com>
2024-05-01 20:32:45 +0530

    py-c communicate code

---
+++ Functionalities/-Python-C Communication/py-c/c_program1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int get_input_from_user() {
+    int input;
+    printf("Enter a number: ");
+    scanf("%d", &input);
+    return input;
+}
---
+++ Functionalities/-Python-C Communication/py-c/c_program2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void print_value_from_python(int value) {
+    printf("Value received from Python: %d\n", value);
+}
